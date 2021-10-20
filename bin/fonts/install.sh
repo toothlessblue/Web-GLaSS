1376b2c293ff971b5a3d1f75c977e11282f4189a
Ellis Verhoest <ellis@lupimedia.com>
2021-10-20 23:22:45 +0100

    naughty gitdoall

--- bin/fonts/install.sh
+++ bin/fonts/install.sh
@@ -1,9 +1,12 @@
 echo "Requires sudo privelages"
 echo "Runs:"
 echo "- sudo apt update"
+echo "- pip install --upgrade pip"
 echo ""
 echo "Installs:"
 echo "- python3"
+# echo "- fonttools (py)"
+echo "- Pillow (py)"
 echo ""
 read -p "Continue? " -n 1 -r
 echo    # (optional) move to a new line
@@ -14,3 +17,7 @@ fi
 
 sudo apt update
 sudo apt install python3
+# pip3 install fonttools
+
+python3 -m pip install --upgrade pip
+python3 -m pip install --upgrade Pillow
--- bin/fonts/process-font.py
+++ bin/fonts/process-font.py
@@ -1,20 +1,47 @@
 # TODO this but properly, in a seperate project in C++ that this project clones and then installs, so I can compile an executable and don't have to rely on ol' unreliable python
 
-import argparse;
-import sys;
+import argparse
+import sys, os, shutil
+from fontTools.ttLib import TTFont
+from PIL import Image, ImageFont, ImageDraw
 
 parser = argparse.ArgumentParser(description='Converts an input TTF font file to a texture and UV map, outputs a directory containing both files')
 parser.add_argument('inputFilePath', metavar='Input File', nargs=1, help='TTF file to process')
-parser.add_argument('-o', metavar='Output Dir', dest='outputFilePath', nargs=1, help='Where to save the output directory')
+parser.add_argument('-o', dest='outputFilePath', nargs=1, help='Where to save the output directory')
+parser.add_argument('-r', '--resolution', default=1024, dest='resolution', nargs=1, help='Resolution of the output texture')
+parser.add_argument('-O', '--overwrite', dest='overwrite', nargs='?', const=True, help='Delete the output directory if it already exists')
 
 args = parser.parse_args()
 
-inputData = None
+# Prepare output directory
+outputPath = args.outputFilePath or 'bin/fonts/out/'
+dirPath = outputPath + args.inputFilePath[0].split('/')[-1] + '/'; 
+
+if os.path.isdir(dirPath):
+    if args.overwrite:
+        print('Output directory already exists, overwriting')
+        shutil.rmtree(dirPath)
+    else:
+        print('Output directory already exists, run with -O to override')
+        sys.exit(os.EX_OSFILE)
+
+os.mkdir(dirPath)
+
+# Render font to texture
+font = None
 
 try:
-    with open(args.inputFilePath[0], "r") as inputFile:
-        inputData = inputFile.read()
+    #font = ImageFont.truetype(args.inputFilePath[0], 15)
+    font = TTFont(args.inputFilePath[0])
 except FileNotFoundError:
     print('Input file does not exist.')
-    sys.exit(1) # Couldn't find a "file not found" exit status - 1 is general catchall apparently
+    sys.exit(os.EX_NOINPUT)
 
+print(font.getGlyphID(font.getGlyphNames()[1]))
+exit()
+
+
+texture = Image.new('RGBA', (args.resolution, args.resolution))
+draw = ImageDraw.Draw(texture)
+# Save files
+texture.save(dirPath + '/atlas.png')
