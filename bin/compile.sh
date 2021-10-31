d85baaf3f5f3ce9dd0b7e3f4bdd125244c18a048
Ellis Verhoest <ellis.verhoest@gmail.com>
2021-10-31 20:36:19 +0000

    started using make, yay for a shitty life

--- bin/compile.sh
+++ bin/compile.sh
@@ -11,36 +11,33 @@ cp -R ./src/WebPage/* ./lib/
 echo "- all resource folders"
 find . -wholename "**/resources/*" -type d -prune -exec cp -R "{}" "./lib/resources/" \;
 
-               # Find all cpp files in   # Exclude any file             # Replace newlines
-               # src/                    # in a .vshistory folder       # with space
+               # Find all cpp files in   # Exclude any file           # Replace newlines
+               # src/                    # in a .vshistory folder     # with space
 CPP_FILEPATHS=$(find src/ -name "*.cpp" -not -path "*/\.vshistory/*" | tr '\n' ' ') 
 
-echo ""
-echo "Compiling files: "
-echo ${CPP_FILEPATHS}
-echo ""
-
-# TODO use make instead, will compile faster and lets us use a better incremental compiler
-#   This will also allow disabling warnings like "warning: nested namespace definition is a C++17 extension; define each namespace separately"
-#   Keep trying to complete this todo but always give up cause make looks like such a pain
-
-# Uses WebGL 2, this is newer and has less support but gives us access to a bunch of fun tools
-#  like compute shaders
-
-echo "Running emcc..."
-./emsdk/upstream/emscripten/emcc ${CPP_FILEPATHS}   `# Compile all c++ files` \
--fexceptions                                        `# Enable java script based c++ exceptions` \
-`#-fwasm-exceptions                                  # Enable wasm based c++ exceptions - Faster but lacks support - https://webassembly.org/roadmap/` \
--O0                                                 `# The optimisation setting, O0 is slowest to run but fastest to compile, O3 is fastest to run but slowest to compile` \
--s LLD_REPORT_UNDEFINED                             `# Report errors for undefined access to variables (I think? It's handy)` \
--s WASM=1                                           `# Build to web assembly instead of pure js` \
--s MAX_WEBGL_VERSION=2                              `# Use WebGL 2 - Compute shaders + optimised` \
--s USE_GLFW=3                                       `# GL version to use` \
--o ./lib/index.js                                   `# Output file, specifying .js outputs index.wasm and index.js files` \
--fsanitize=undefined                                `# Much needed more detailed error info` \
---preload-file ./lib/resources@/                    `# Data files to include in the virtual file system, adds index.data to output` \
+# Creates the Makefile.am file
+echo "AUTOMAKE_OPTIONS = foreign
+bin_PROGRAMS = index
+AM_LDFLAGS = -fexceptions \
+-O0 \
+-s LLD_REPORT_UNDEFINED \
+-s WASM=1 \
+-s MAX_WEBGL_VERSION=2 \
+-s USE_GLFW=3 \
+-o ./lib/index.js \
+-fsanitize=undefined \
+--preload-file ./lib/resources@/ \
 -L./external-libs -lfreetype
+index_SOURCES=$CPP_FILEPATHS" > Makefile.am
+
+echo "Running emmake..."
+emsdk/upstream/emscripten/emmake make
 
 echo ""
 echo "Compilation complete"
 echo ""
+
+# Move output files to lib folder - TODO could probably specify this in the makefile.am but w/e
+mv index lib/index.js
+mv index.data lib/
+mv index.wasm lib/
